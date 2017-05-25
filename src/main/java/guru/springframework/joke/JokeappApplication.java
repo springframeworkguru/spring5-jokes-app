5c9f44a7676efd5a131ed7f7ba0546d9b99317e6
John Thompson <john@springframework.guru>
2017-05-25 14:20:40 -0400

    init add of project

---
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeappApplication.class, args);
+	}
+}
